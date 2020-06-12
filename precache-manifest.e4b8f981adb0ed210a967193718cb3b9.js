af590ea577253753241389692164a2fbf8dcb75f
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-06-12 22:07:31 +0530

    Updates

---
+++ precache-manifest.e4b8f981adb0ed210a967193718cb3b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1d426c0a33e7fad353ac999b5fefa0",
+    "url": "/TodoApp-React-Redux-Hooks/index.html"
+  },
+  {
+    "revision": "d400a40c3138993bf1d1",
+    "url": "/TodoApp-React-Redux-Hooks/static/css/main.2f0fc497.chunk.css"
+  },
+  {
+    "revision": "4ee8df70bbf3e85318d8",
+    "url": "/TodoApp-React-Redux-Hooks/static/js/2.eca55b5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoApp-React-Redux-Hooks/static/js/2.eca55b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d400a40c3138993bf1d1",
+    "url": "/TodoApp-React-Redux-Hooks/static/js/main.856aa93a.chunk.js"
+  },
+  {
+    "revision": "06d6739e3f8d18bc4299",
+    "url": "/TodoApp-React-Redux-Hooks/static/js/runtime-main.6a699c79.js"
+  }
+]);
